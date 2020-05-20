72bc091ade734c8a955b7e22f24e4632e2467829
manxingxing <manxingxing@gmail.com>
2020-05-20 21:35:13 +0800

    fix: 使用 webpack.ProvidePlugin 前要先require('webpack')

--- webpacker.rb
+++ webpacker.rb
@@ -1,10 +1,10 @@
 # 覆盖方法，让 rails 从当前文件所在路径查找模版
 source_paths << __dir__
 
-rails_command 'webpacker:install'
-
 run 'yarn add jquery'
 run 'yarn add expose-loader'
+
+rails_command 'webpacker:install'
 
 run 'mkdir -p app/javascript/images'
 
@@ -13,6 +13,8 @@ gsub_file "app/javascript/packs/application.js", "// const imagePath", "const im
 
 copy_file 'webpacker/loaders/jquery.js', 'config/webpack/loaders/jquery.js'
 copy_file 'webpacker/loaders/ujs.js', 'config/webpack/loaders/ujs.js'
+
+prepend_file 'config/webpack/environment.js', "const webpack = require('webpack');\n"
 
 inject_into_file 'config/webpack/environment.js', after: "const { environment } = require('@rails/webpacker')\n" do
 <<-CODE
